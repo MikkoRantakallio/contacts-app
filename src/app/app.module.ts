c7901a7afb362edd6c36437f74e6037e1bf56e38
MikkoRantakallio <mikko.rantakallio@gmail.com>
2017-11-06 13:21:06 +0200

    New component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,34 @@ import { ContactListItemComponent } from './contact/contact-list/contact-list-it
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import {ContactService} from './contact/services/contact.service';
 import {MaterialComponentsModule} from './material-components/material-components.module';
+import {RouterModule, Routes} from '@angular/router';
+import {AddContactComponent} from './contact/add-contact/add-contact.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+const routes: Routes = [
+  {
+    path: 'add-contact',
+    component: AddContactComponent
+  },
+  {
+    path: 'contacts',
+    component: AppComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListItemComponent,
-    ContactListComponent
+    ContactListComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
-    MaterialComponentsModule
+    MaterialComponentsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
   ],
   providers: [
     ContactService
